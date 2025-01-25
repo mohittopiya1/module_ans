d083465b2d194e191cd90659786516a5488b1540
mohittopiya1 <mohit.topiya.207@gmail.com>
2025-01-25 19:55:34 +0530

    Add files via upload

---
+++ module-1/module-1.cpp
@@ -0,0 +1,14 @@
+// MODULE - 1
+
+#include<stdio.h>
+
+main ()
+
+{
+	
+printf("name: Mohit Topiya");
+printf("\nbirth date: 20-07-2004");
+printf("\nage: 21");
+printf("\naddress: pruthvi tower");
+
+}
